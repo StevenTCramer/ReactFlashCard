67bfff284330da0431ab6f9bd9324ac41a7700de
StevenTCramer <>
2016-12-13 05:13:01 -0500

    Change Typescript version and formatting

--- Source/FlashCards/App/FlashMe.tsx
+++ Source/FlashCards/App/FlashMe.tsx
@@ -1,45 +1,45 @@
 ﻿import * as React from 'react';
 
 export class FlashMe extends React.Component<{}, {}> {
-    thaiFlashDeck: FlashDeck;
-    XmlHttpRequest: XMLHttpRequest;
+  thaiFlashDeck: FlashDeck;
+  XmlHttpRequest: XMLHttpRequest;
 
 
-    componentDidMount = (): void => {
-        this.getData();
-    }
+  componentDidMount = (): void => {
+    this.getData();
+  }
 
-    componentWillUnmount = (): void => {
-        this.XmlHttpRequest.abort();
-    }
+  componentWillUnmount = (): void => {
+    this.XmlHttpRequest.abort();
+  }
 
 
-    render() {
-        var divStyle: React.CSSProperties = {
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            background: "pink"
-        };
+  render() {
+    var divStyle: React.CSSProperties = {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "pink"
+    };
 
-        return (
-            <div style={divStyle}>FlashMe</div>
-        );
-    }
+    return (
+      <div style={divStyle}>FlashMe</div>
+    );
+  }
 
-    getData = (): void => {
-        this.XmlHttpRequest = new XMLHttpRequest();
-        this.XmlHttpRequest.open('GET', encodeURI('/FlashCardApplication/FlashCardApplicationFeature'));
-        this.XmlHttpRequest.onload = function () {
-            if (this.XmlHttpRequest.status === 200) {
-                var data = JSON.parse(this.XmlHttpRequest.responseText);
-                this.setState(data);
-            }
-            else {
-                alert('Request failed.  Returned status of ' + this.XmlHttpRequest.status);
-            }
-        };
-        this.XmlHttpRequest.send();
-    }
+  getData = (): void => {
+    this.XmlHttpRequest = new XMLHttpRequest();
+    this.XmlHttpRequest.open('GET', encodeURI('/FlashCardApplication/FlashCardApplicationFeature'));
+    this.XmlHttpRequest.onload = () => {
+      if (this.XmlHttpRequest.status === 200) {
+        var data = JSON.parse(this.XmlHttpRequest.responseText);
+        this.setState(data);
+      }
+      else {
+        alert('Request failed.  Returned status of ' + this.XmlHttpRequest.status);
+      }
+    };
+    this.XmlHttpRequest.send();
+  }
 }
